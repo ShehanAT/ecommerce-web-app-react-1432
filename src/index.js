9b54e20f91309a28ae2f400eab625e443cbd899f
Nipun <shehanatuk@gmail.com>
2022-06-06 11:44:58 -0400

    fixed src/index.js errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root');
-// root.render(
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
